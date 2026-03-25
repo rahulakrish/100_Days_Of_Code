48e62cccbd8c2f9ed53537ddcdb1db3b71a1f2ae
rahul_krishnan <rahulakrish@gmail.com>
2026-03-25 10:49:27 -0500

    Add files via upload

---
+++ Day 35 - API Authentication/rain_alert_workflow.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ### Modifying the twilio code for Github workflow instead
+
+# In[6]:
+
+
+import json
+import requests
+import os 
+import smtplib
+
+
+# In[2]:
+
+
+#retrieve environment variables
+weather_api_key = os.environ.bet('OWM_API_KEY')
+
+
+# In[3]:
+
+
+#set lat & long to a location where it's raining
+lat = 30.406401
+long = -87.682083
+
+
+# In[7]:
+
+
+#getting the data from the open weather API
+api_call= f"https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={long}&appid={weather_api_key}&units=metric"
+response = requests.get(url=api_call)
+weather_data = response.json()
+
+
+# In[33]:
+
+
+#if it's raining bring an umbrella.
+#here, create a flag that is set to true.
+#this way, only one print statement gets printed if it's raining within the next 12 hours
+will_rain = False
+
+#setting the range because you only need to check for the next 12 hours.
+for i in range(0, 4):
+    #weather id's b/w 500 and 600 denote raing per the API docs
+    if 500 <= weather_data['list'][i]['weather'][0]['id'] < 600:
+        will_rain = True
+
+        
+        
+def send_alert():
+    from_email = os.environ,get('FROM_EMAIL')
+    to_email = os.environ.get('TO_EMAIL')
+    password = os.environ.get('PASSWORD')
+    
+    with smtplib.SMTP('smtp.gmail.com', 587) as connection:
+        connection.starttls()
+        connection.login(user=from_email, password=password)
+        connection.sendemail(from_addr = from_email,
+                             to_addr = to_email,
+                             msg = "Subject: Rain Alert\n\nForecast is for rain. Don't forget to take an umbrella")
+        
+
+if will_rain = True:
+    send_alert()
+    
+
